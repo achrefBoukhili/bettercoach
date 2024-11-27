fda01739907dedca4ac1e412364455e0e300d31b
Achref Boukhili <achref.boukhili.dev+github@gmail.com>
2024-11-27 11:31:08 +0100

    prepare deploy for vercel

--- src/components/error-boundary/error-boundary.tsx
+++ src/components/error-boundary/error-boundary.tsx
@@ -1,25 +1,25 @@
 import React from "react"
 
 class ErrorBoundary extends React.Component {
-  constructor(props) {
+  constructor(props: any) {
     super(props)
 
     // Define a state variable to track whether is an error or not
     this.state = { hasError: false }
   }
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI
 
     return { hasError: true }
   }
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch(error: any, errorInfo: any) {
     // You can use your own error logging service here like sentry
     console.log({ error, errorInfo })
   }
   render() {
     // Check if the error is thrown
+
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return (
         <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full bg-white">
           <h1 className="font-bold text-gray-400 text-9xl animate__animated animate__bounce">
